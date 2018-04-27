fa1cf712137a89e872aaad2d2e29cc64b04138e6
rbrazioli <renato.brazioli@gmail.com>
2018-04-27 17:48:14 +0200

    Cookies on!
    Add Cookies notice

---
+++ js/cookies_banner.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#2c3e50",
+      "text": "#ffffff"
+    },
+    "button": {
+      "background": "#d96321",
+      "text": "#ffffff"
+    }
+  },
+  "theme": "edgeless",
+  "content": {
+    "message": "Questo sito utilizza cookie, anche di terze parti,
+    per migliorare l'esperienza d'uso. Procedendo con la navigazione
+    acconsenti all'uso dei cookie.",
+    "dismiss": "OK!",
+    "link": "Approfondisci"
+  }
+})});
