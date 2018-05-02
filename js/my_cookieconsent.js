e818d63db708d00ad11667660d16005c71dfd309
rbrazioli <renato.brazioli@gmail.com>
2018-05-02 16:01:34 +0200

    Refactor
    Used better naming for files and moved js for Cookie consent out of
    main file

---
+++ js/my_cookieconsent.js
@@ -0,0 +1,25 @@
+/* my_cookieconsent
+ * customization of cookieconsent for the presente website
+ * me@renatobrazioli.it 2-MAY-2018 - create
+ */
+
+  window.addEventListener("load", function(){
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#2c3e50",
+        "text": "#ffffff"
+      },
+      "button": {
+        "background": "#d96321",
+        "text": "#ffffff"
+      }
+    },
+    "theme": "edgeless",
+    "content": {
+      "message": "Questo sito utilizza cookie, anche di terze parti, per migliorare l'esperienza d'uso. Procedendo con la navigazione acconsenti all'uso dei cookie.",
+      "dismiss": "OK!",
+      "link": "Approfondisci",
+      "href": "http://renatobrazioli.it/info_cookie.html"
+    }
+  })});
